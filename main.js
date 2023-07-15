fe040c556ba1ee0e522c36f20d4a39dbd6bbeed7
CatherinZ <317358092@quimica.unam.mx>
2023-07-15 03:21:48 -0600

    mi primer comentario

---
+++ main.js
@@ -0,0 +1,15 @@
+var app = document.getElementById("app");
+ var  typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString("Hola mundo")
+  .pauseFor(300)
+  .deleteChars(5)
+  .typeString("<strong>Techolochicas,</strong> ")
+  .typeString('<strong><span style="color: red;">Saludos</span> Cordiales!</strong>')
+  .pauseFor(1000)
+  .start();
